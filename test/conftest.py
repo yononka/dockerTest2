39ad0222fcf02667a89633a91a48bc889f37fca8
anya_s <yodevdeals@gmail.com>
2020-07-07 15:56:14 +0300

    now chrome is headless

--- test/conftest.py
+++ test/conftest.py
@@ -3,9 +3,14 @@ from selenium import webdriver
 from selenium.webdriver.chrome.webdriver import WebDriver
 from webdriver_manager.chrome import ChromeDriverManager
 
+
 @pytest.fixture
 def browser():
-  driver: WebDriver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
-  driver.implicitly_wait(10)
-  yield driver
-  driver.quit()
+    chrome_options = webdriver.ChromeOptions()
+    # chrome_options.add_argument('--no-sandbox')
+    # chrome_options.add_argument('--window-size=1420,1080')
+    chrome_options.add_argument('--headless')
+    driver: WebDriver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), chrome_options=chrome_options)
+    driver.implicitly_wait(10)
+    yield driver
+    driver.quit()
--- test/test_sample.py
+++ test/test_sample.py
@@ -2,4 +2,5 @@ def test_sample(browser):
     browser.get("http://www.google.com")
     title = "Google"
     print("yep, it's running...")
+    # screenshot = browser.save_screenshot('test.png')
     assert title == browser.title
