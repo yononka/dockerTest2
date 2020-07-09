3fec217611a8cf08cba346988caa0457c3252393
anya_s <yodevdeals@gmail.com>
2020-07-09 12:58:44 +0300

    update

--- test/conftest.py
+++ test/conftest.py
@@ -6,9 +6,12 @@ from selenium.webdriver.chrome.webdriver import WebDriver
 @pytest.fixture
 def browser():
     chrome_options = webdriver.ChromeOptions()
-    # chrome_options.add_argument('--no-sandbox')
     # chrome_options.add_argument('--window-size=1420,1080')
     chrome_options.add_argument('--headless')
+    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--incognito")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--no-sandbox")
     # driver: WebDriver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=chrome_options)
     driver = webdriver.Chrome("../chromedriver", options=chrome_options)
     driver.implicitly_wait(10)
