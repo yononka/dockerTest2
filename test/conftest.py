f1b9892c6fc2f8e00fea4358e270971bd5e8738c
anya_s <yodevdeals@gmail.com>
2020-07-09 12:44:38 +0300

    update

--- test/conftest.py
+++ test/conftest.py
@@ -1,8 +1,7 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.webdriver import WebDriver
-from webdriver_manager.chrome import ChromeDriverManager
-
+# from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture
 def browser():
@@ -10,7 +9,8 @@ def browser():
     # chrome_options.add_argument('--no-sandbox')
     # chrome_options.add_argument('--window-size=1420,1080')
     chrome_options.add_argument('--headless')
-    driver: WebDriver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=chrome_options)
+    # driver: WebDriver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=chrome_options)
+    driver = webdriver.Chrome("../chromedriver", options=chrome_options)
     driver.implicitly_wait(10)
     yield driver
     driver.quit()
